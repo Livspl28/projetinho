a43cd50af941d7715edb901b239ca5530ade3550
Livspl28 <livstml28@gmail.com>
2026-07-31 16:46:25 -0300

    Add files via upload

---
+++ insert.php
@@ -0,0 +1,21 @@
+<?php
+require_once 'crud.php';
+
+$novoCurriculo = [
+    'nome' => $_POST['nome'],
+    'cargo' =>  $_POST['cargo'],
+    'bio' =>  $_POST['bio'],
+    'princi' =>  $_POST['princi'],
+    'email' =>  $_POST['email'],
+    'telefone' =>  $_POST['telefone'],
+    'perfis_profis' =>  $_POST['perfis_profis'],
+    'info_princi' =>  $_POST['info_princi'],
+    'empresa' =>  $_POST['empresa'],
+    'funcao' =>  $_POST['funcao'],
+    'descricao' =>  $_POST['descricao'],
+    'curso' =>  $_POST['curso'],
+    'funcao' =>  $_POST['funcao'],
+
+
+    'imagem' => ''
+];
